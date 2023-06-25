922bbdc047733c1ec0078155440164059e13209a
Asiak <asiakemp24@gmail.com>
2023-06-25 18:36:52 -0400

    created prompt questions

--- index.js
+++ index.js
@@ -3,14 +3,42 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // Write code for a prompt input
+inquirer.prompt([
+    {
+        // Enter 3 character prompt
+        type:'input',
+        message:'Please give up to 3 characters for your logo',
+        name:'characters'
+    },
+    {
+        // Enter a color keyword
+        type:'input',
+        message:'Please type in the name of the color for the characters',
+        name:'charColor'
+    },
+    {
+        // Enter a shape from a circle, triange, and square
+        type: 'list',
+        message:'Please select shape of logo',
+        name:'shape',
+        choices:['circle', 'triangle', 'square']
+    },
+    {
+        // Enter shape color keyword
+        type:'input',
+        message:'Please select a color for the shape of the logo',
+        name:'shapeColor',
+    }
+    
+])
 
-// Enter 3 character prompt
 
-// Enter a color keyword
 
-// Enter a shape from a circle, triange, and square
 
-// Enter shape color keyword
+
+
+
+// 
 
 // creates the logo.svg in command line and opens in a browser
 // these go into the examples folder
