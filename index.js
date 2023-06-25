35e7ad2c691a35a65d75f40c2059430d2f8c4537
Asiak <asiakemp24@gmail.com>
2023-06-25 18:27:43 -0400

    installed needed dependencies

---
+++ index.js
@@ -0,0 +1,16 @@
+// use inquirer and call packages needed
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// Write code for a prompt input
+
+// Enter 3 character prompt
+
+// Enter a color keyword
+
+// Enter a shape from a circle, triange, and square
+
+// Enter shape color keyword
+
+// creates the logo.svg in command line and opens in a browser
+// these go into the examples folder
